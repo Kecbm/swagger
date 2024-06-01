6306f79359810665e7f9722dcff2a535f7a8e5d6
Klecianny Melo <kleciannymelo@gmail.com>
2024-06-01 20:10:32 -0300

    :sparkles: limitando as rotas que estão disponíveis no Swagger

--- instance/src/instance/instance.controller.ts
+++ instance/src/instance/instance.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { InstanceService } from './instance.service';
 import { CreateInstanceDto } from './dto/create-instance.dto';
 import { UpdateInstanceDto } from './dto/update-instance.dto';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller('instance')
 export class InstanceController {
   constructor(private readonly instanceService: InstanceService) {}
 
   @Post()
+  @ApiExcludeEndpoint()
   create(@Body() createInstanceDto: CreateInstanceDto) {
     return this.instanceService.create(createInstanceDto);
   }
@@ -18,12 +28,17 @@ export class InstanceController {
   }
 
   @Get(':id')
+  @ApiExcludeEndpoint()
   findOne(@Param('id') id: string) {
     return this.instanceService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInstanceDto: UpdateInstanceDto) {
+  @ApiExcludeEndpoint()
+  update(
+    @Param('id') id: string,
+    @Body() updateInstanceDto: UpdateInstanceDto,
+  ) {
     return this.instanceService.update(+id, updateInstanceDto);
   }
 
